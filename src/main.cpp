b047fa649e9f3a71ca010e585ff21d417fd39ee1
heissendo <heissendo88@gmail.com>
2024-12-29 18:45:22 +0200

    clean up code and add comments2

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,6 @@ size_t writeCallback(char *content, size_t size, size_t nmemb, std::string* user
     return realSize;
 }
 
-size_t findPos(std::string& data, size_t lastPos, std::string& delimiter){
-
-}
 
 int main() {
 
@@ -21,34 +18,36 @@ int main() {
     CURL *curl;
     std::string responseData;
     
-    std::vector<std::string> headlines;
+    // sw and lastchar for headlinesearch, url for destination
     std::string searchWord = "class=\"front-title\">";
     std::string lastChar = "<";
+    const char* url = "https://www.iltalehti.fi";
     size_t lastPos = 0;
-    unsigned int headlineLength = 35;
+    std::vector<std::string> headlines;
 
+ 
 
     curl = curl_easy_init();
 
     if (curl){
-    curl_easy_setopt(curl, CURLOPT_URL, "https://www.iltalehti.fi");
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseData); 
-    
-    res = curl_easy_perform(curl);
-
-
-    while ( lastPos != std::string::npos){
-        lastPos = responseData.find(searchWord, lastPos);
-
-        if (lastPos == std::string::npos){
-            continue;
-        }   
-        size_t lastCharPos = responseData.find(lastChar, lastPos); 
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseData); 
         
-        headlines.push_back(responseData.substr(lastPos + searchWord.size(), lastCharPos - (lastPos + searchWord.size())));
-        lastPos += searchWord.size();
-          
+        res = curl_easy_perform(curl);
+
+        // go trough response and save every headline to vector
+        while ( lastPos != std::string::npos){
+            lastPos = responseData.find(searchWord, lastPos);
+
+            if (lastPos == std::string::npos){
+                continue;
+            }   
+            size_t lastCharPos = responseData.find(lastChar, lastPos); 
+            
+            headlines.push_back(responseData.substr(lastPos + searchWord.size(), lastCharPos - (lastPos + searchWord.size())));
+            lastPos += searchWord.size();
+            
     }
 
 
