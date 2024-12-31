65c118d8952b091e02c9348584f1319684198fa6
heissendo <heissendo88@gmail.com>
2024-12-31 13:50:13 +0200

    write url extraction

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,12 @@
 #include <string>
 #include <vector>
 
+struct hl
+{
+    std::string headline;
+    std::string headlineUrl;
+};
+
 
 size_t writeCallback(char *content, size_t size, size_t nmemb, std::string* userData){
     size_t realSize = size * nmemb;
@@ -23,7 +29,7 @@ int main() {
     std::string lastChar = "<";
     const char* url = "https://www.iltalehti.fi";
     size_t lastPos = 0;
-    std::vector<std::string> headlines;
+    std::vector<hl> headlines;
 
  
 
@@ -43,17 +49,29 @@ int main() {
             if (lastPos == std::string::npos){
                 continue;
             }   
-            size_t lastCharPos = responseData.find(lastChar, lastPos); 
+
+            size_t lastCharPos = responseData.find(lastChar, lastPos);
+
+            std::string hlTemp = responseData.substr(lastPos + searchWord.size(), lastCharPos - (lastPos + searchWord.size()));
             
-            headlines.push_back(responseData.substr(lastPos + searchWord.size(), lastCharPos - (lastPos + searchWord.size())));
+            // below search for headline url
+            size_t hlUrlPos = responseData.rfind("<a href=\"", lastPos);
+            size_t hlUrllastPos = responseData.find("\">", hlUrlPos);
+            
+            std::string hlUrlTemp = responseData.substr(hlUrlPos + 10, hlUrllastPos - (hlUrlPos + 10));
+            
+            hl tmpHeadline = {hlTemp, hlUrlTemp};
+            headlines.push_back(tmpHeadline);
+            
             lastPos += searchWord.size();
             
     }
 
 
     for (const auto &i : headlines){
-        std::cout << i << std::endl;
+        std::cout << i.headline << " - " << i.headlineUrl << std::endl;
     }
+
 
     curl_easy_cleanup(curl);
     curl = NULL;
