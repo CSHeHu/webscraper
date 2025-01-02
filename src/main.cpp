3522e5c7c0b4c15de92db3bfbb621d7a5945a5cc
heissendo <heissendo88@gmail.com>
2025-01-02 23:56:08 +0200

    write basic qt stuff and modify cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,23 @@
 #include "dataManager.h"
+#include "mainwindow.h"
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-/*
+
+    DataManager data;
+    data.updateData();
+
+    for (const auto &i : data.getHeadlines())
+    {
+        std::cout << i.headline << " - " << i.headlineUrl << std::endl;
+    }
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     return a.exec();
-*/
 
-DataManager data;
-data.updateData();
 
-for (const auto &i : data.getHeadlines())
-{
-    std::cout << i.headline << " - " << i.headlineUrl << std::endl;
-}
 
 }
